f113dcd5db746652e65c656a6f0993b32bd69352
Joel Stimson <stimsojr@gmail.com>
2012-01-21 00:38:15 -0800

    split out tests for helper methods from more integration-realted tests. added spec helper file to hold simplecov start.

--- lib/uncoil.rb
+++ lib/uncoil.rb
@@ -7,76 +7,76 @@ class Uncoil
   ISGD_ROOT_URL = "http://is.gd/forward.php?format=json&shorturl="
   BITLY_DOM_ARRAY = %w[bit.ly, j.mp, bitlypro.com, cs.pn, nyti.ms]
   
-    def initialize options = {}
-      Bitly.use_api_version_3
-      @bitly_access = false
+  def initialize options = {}
+    Bitly.use_api_version_3
+    @bitly_access = false
+    
+    # create bitly instance if the auth criteria are all entered by user
+    if options.has_key?(:bitlyuser) && options.has_key?(:bitlykey)
+      @bitly_instance = Bitly.new("#{options[:bitlyuser]}", "#{options[:bitlykey]}")
+      @bitly_access = true
+    end
+  end
+
+  def expand url_arr
+    output_array = Array(url_arr).flatten.map do |short_url|
+      short_url = clean_url(short_url)
+      domain    = identify_domain(short_url)
       
-      if options.has_key?(:bitlyuser) && options.has_key?(:bitlykey)
-        @bitly_instance = Bitly.new("#{options[:bitlyuser]}", "#{options[:bitlykey]}")
-        @bitly_access = true
-      end
-    end
-
-    def identify_domain short_url
-      clean_url(short_url).split("/")[2].to_s
-    end
-
-    def clean_url short_url
-      short_url = "http://" << short_url unless short_url =~ /^https?:\/\//
-      short_url.chop! if short_url[-1] == "/"
-      short_url
-    end
-
-    def expand url_arr
-      out_arr = Array(url_arr).flatten.map do |short_url|
-        short_url = clean_url(short_url)
-        domain    = identify_domain(short_url)
-        
-        # do i really need this? i dont think it gets hit correctly
-        @bitly_access.nil? ? error = "Not logged in to Bitly API" : error = nil
-        
-        begin
+      begin
+        long_url = 
           if @bitly_access && ( BITLY_DOM_ARRAY.include?(domain) || check_bitly_pro(domain) )
-            long_url = uncoil_bitly(short_url)
+            uncoil_bitly(short_url)
           elsif domain == "is.gd"
-            long_url = uncoil_isgd(short_url)
+            uncoil_isgd(short_url)
           else
-            long_url = uncoil_other(short_url)
+            uncoil_other(short_url)
           end
-        rescue => exception
-          long_url = nil
-          error = exception.message
-        end
-        
-        # { :short_url => short_url , :long_url => long_url, :error => error }
-        Response.new(long_url, short_url, error)
+      rescue => exception
+        long_url = nil
+        error = exception.message
       end
-      
-      out_arr.length == 1 ? out_arr[0] : out_arr
-
+      # return a response object for each time through the loop
+      Response.new(long_url, short_url, error)
     end
+    # here's the return
+    output_array.length == 1 ? output_array[0] : output_array
+  end
 
-    def check_bitly_pro url_domain
-      @bitly_instance.bitly_pro_domain(url_domain)
-    end
+  def check_bitly_pro url_domain
+    @bitly_instance.bitly_pro_domain(url_domain)
+  end
 
-    def uncoil_bitly short_url
-      @bitly_instance.expand(short_url).long_url
-    end
+  def uncoil_bitly short_url
+    @bitly_instance.expand(short_url).long_url
+  end
 
-    def uncoil_isgd short_url
-      JSON.parse(open(ISGD_ROOT_URL + "#{short_url}") { |file| file.read } )["url"]
-    end
+  def uncoil_isgd short_url
+    JSON.parse(open(ISGD_ROOT_URL + "#{short_url}") { |file| file.read } )["url"]
+  end
 
-    def uncoil_other short_url, depth = 10
-      url = URI.encode(short_url)
-      response = Net::HTTP.get_response(URI.parse(url))
-      
-      case response
-        when Net::HTTPSuccess     then url
-        when Net::HTTPRedirection then uncoil_other(response['location'], depth - 1)
-      end
+  def uncoil_other short_url, depth = 10
+    url = URI.encode(short_url)
+    response = Net::HTTP.get_response(URI.parse(url))
+    
+    case response
+      when Net::HTTPSuccess     then url
+      when Net::HTTPRedirection then uncoil_other(response['location'], depth - 1)
     end
+  end
+  
+  # helper methods to clean up the code above
+  
+  def identify_domain short_url
+    clean_url(short_url).split("/")[2].to_s
+  end
+
+  def clean_url short_url
+    short_url = "http://" << short_url unless short_url =~ /^https?:\/\//
+    short_url.chop! if short_url[-1] == "/"
+    short_url
+  end
+  
 end
 
 class Uncoil::Response
--- spec/uncoil_spec.rb
+++ spec/uncoil_spec.rb
@@ -1,100 +1,40 @@
-require 'simplecov'
-SimpleCov.start
+require 'spec_helper'
 require 'uncoil'
 
 describe Uncoil do
   
   subject { Uncoil.new(:bitlyuser => "stim371", :bitlykey => "R_7a6f6d845668a8a7bb3e0c80ee3c28d6")}
   
-  context "when cleaning up the url" do
-    
-    it "should add the prefix if none exists" do
-      subject.clean_url("cnn.com").should eq "http://cnn.com"
-      subject.clean_url("cnn.com/").should eq "http://cnn.com"
-    end
-    
-    it "should not add the prefix if one exists" do
-      subject.clean_url("http://cnn.com").should eq "http://cnn.com"
-      subject.clean_url("http://cnn.com/").should eq "http://cnn.com"
-    end
-    
-    it "should not add the prefix if a secure one exists" do
-      subject.clean_url("https://cnn.com").should eq "https://cnn.com"
-      subject.clean_url("https://cnn.com/").should eq "https://cnn.com"
-    end
-    
-    it "should remove the trailing slash" do
-      subject.clean_url("http://cnn.com/").should eq "http://cnn.com"
-      subject.clean_url("cnn.com/").should eq "http://cnn.com"
-    end
-    
-    it "should not remove characters on the end that aren't slashes" do
-      subject.clean_url("http://cnn.com").should eq "http://cnn.com"
-      subject.clean_url("cnn.com").should eq "http://cnn.com"
-    end
-    
-  end
-  
-  context "when extracting the domain" do
-  
-    it "should correctly extract the domain from a normal url" do
-      subject.identify_domain("http://bit.ly/2EEjBl").should eq "bit.ly"
-    end
-  
-  end
-  
-  context "when checking a domain" do
-    
-    it "should identify pro domains" do
-      subject.check_bitly_pro("cs.pn").should be_true
-      subject.check_bitly_pro("nyti.ms").should be_true
-    end
-    
-    it "should identify non-pro domains" do
-      subject.check_bitly_pro("bit.ly").should be_false #the bit.ly domain comes back false since its not a "pro" domain
-      subject.check_bitly_pro("tinyurl.com").should be_false
-    end
-    
-  end
-  
   describe "when using the submethods" do
     
     context "when trying to undo a bit.ly link" do
-    
       it "should bring back the correct long link" do
         expected_result = "http://www.cnn.com/"
         subject.uncoil_bitly("http://bit.ly/2EEjBl").should eq expected_result
       end
-    
     end
   
     context "when trying to undo a bit.ly pro link" do
-    
       it "should bring back the correct long link" do
         expected_result = "http://www.c-spanvideo.org/program/CainNew"
         subject.uncoil_bitly("http://cs.pn/vsZpra").should eq expected_result
       end
-    
     end
   
     context "when trying to undo an is.gd link" do
-    
       it "should bring back the correct long link" do
         subject.uncoil_isgd("http://is.gd/gbKNRq").should eq "http://www.google.com"
       end
-    
     end
   
     context "when trying to undo from other services" do
-    
       it "should bring back the correct long link" do
          subject.uncoil_other("http://tinyurl.com/736swvl").should eq "http://www.chinadaily.com.cn/usa/business/2011-11/08/content_14057648.htm"
       end
-    
     end
   end
   
-  context "the main expand method" do
+  describe "the main expand method" do
     
     it "should return a response object" do
       subject.expand("http://is.gd/gbKNRq").class.should eq Uncoil::Response
@@ -155,9 +95,7 @@ describe Uncoil do
           @expected_result = [Hash[:long_url => "http://www.cnn.com/", :short_url => "http://bit.ly/2EEjBl", :error => nil], Hash[:long_url => "http://www.google.com", :short_url => "http://is.gd/gbKNRq", :error => nil],Hash[:long_url => "http://www.c-spanvideo.org/program/CainNew", :short_url => "http://cs.pn/vsZpra", :error => nil]]
           @response.each_with_index { |response_array, index| check_response response_array, @expected_result[index] }
         end
-        
       end
     end
   end
-  
 end
